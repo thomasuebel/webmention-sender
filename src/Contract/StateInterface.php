67c59902ac43ecb77c76d9436358bd762e8af1be
Thomas Uebel <t.uebel@gmail.com>
2026-03-29 10:02:00 +0200

    feat(state-interface): add assertWritable() to StateInterface

--- src/Contract/StateInterface.php
+++ src/Contract/StateInterface.php
@@ -8,6 +8,15 @@ use WebmentionSender\Exception\StateException;
 
 interface StateInterface
 {
+    /**
+     * Verifies that the state can be persisted by performing a real write.
+     * Call this before sending any webmentions so that a permission problem
+     * is detected upfront rather than after mentions have been dispatched.
+     *
+     * @throws StateException
+     */
+    public function assertWritable(): void;
+
     public function hasBeenSent(string $source, string $target): bool;
 
     /**
